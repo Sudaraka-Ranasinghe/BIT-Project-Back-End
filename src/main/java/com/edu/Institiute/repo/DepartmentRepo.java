08863369ec2f698feaefd9d36bcbe413918f255b
Sudaraka-Ranasinghe <rmssranasinghe@gmail.com>
2025-11-17 14:21:44 +0530

    Department crud

---
+++ src/main/java/com/edu/Institiute/repo/DepartmentRepo.java
@@ -0,0 +1,21 @@
+package com.edu.Institiute.repo;
+
+import com.edu.Institiute.entity.Department;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DepartmentRepo {
+    @Query(value = "SELECT * FROM department WHERE id=:departmentId", nativeQuery = true)
+    Department getAllDepartmentForProvidedId(@Param("departmentId") String departmentId);
+
+    @Query(value = "SELECT * FROM department WHERE id=:departmentId", nativeQuery = true)
+    Optional<Department> getDepartmentById(@Param("departmentId") String departmentId);
+
+//    @Query(value = "SELECT * FROM course WHERE coursename=:courseName", nativeQuery = true)
+//    Optional<Course> findCourseByName(@Param("courseName") String courseName);
+}
