8c5b892ff3e7692f22f6a0a8a0723c6a2a9c7096
Sudaraka-Ranasinghe <rmssranasinghe@gmail.com>
2026-03-08 18:59:20 +0530

    invoice crud

---
+++ src/main/java/com/edu/Institiute/dto/responseDto/paginated/PaginatedResponseInvoiceDto.java
@@ -0,0 +1,16 @@
+package com.edu.Institiute.dto.responseDto.paginated;
+
+import com.edu.Institiute.dto.responseDto.InvoiceResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaginatedResponseInvoiceDto {
+    private Long count;
+    private List<InvoiceResponseDto> dataList;
+}
