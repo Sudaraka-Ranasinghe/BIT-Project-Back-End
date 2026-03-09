39e01faf8e9f95b9fbc3d4fa473dff0ff3e44d38
Sudaraka-Ranasinghe <rmssranasinghe@gmail.com>
2026-03-09 14:52:58 +0530

    Payment save crud operation

---
+++ src/main/java/com/edu/Institiute/dto/responseDto/PaymentResponseDto.java
@@ -0,0 +1,30 @@
+package com.edu.Institiute.dto.responseDto;
+
+import com.edu.Institiute.dto.AdminStaffDto;
+import com.edu.Institiute.dto.InvoiceDto;
+import com.edu.Institiute.dto.StatusDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentResponseDto {
+
+    private String paymentId;
+    private Date paymentDate;
+    private Float amountPaid;
+    private String paymentMethod;
+    private String transactionReference;
+    private String receivedBy;
+    private String paymentCreatedBy;
+    private Date paymentCreatedDate;
+    private String paymentModifiedBy;
+    private Date paymentModifiedDate;
+    private StatusDto Status;
+    private InvoiceDto InvoiceId;
+    private AdminStaffDto AdminStaffId;
+}
---
+++ src/main/java/com/edu/Institiute/dto/responseDto/paginated/PaginatedResponsePaymentDto.java
@@ -0,0 +1,16 @@
+package com.edu.Institiute.dto.responseDto.paginated;
+
+import com.edu.Institiute.dto.responseDto.PaymentResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaginatedResponsePaymentDto {
+    private Long count;
+    private List<PaymentResponseDto> dataList;
+}
